c59fa97f3382c7ee8f0a6bcfcab47e4894674562
hbyyy <qjaduddl94@gmail.com>
2019-12-22 18:19:33 +0900

    m2m homework 수정

--- app/many_to_many/admin.py
+++ app/many_to_many/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from many_to_many.models import Topping, Pizza
+from many_to_many.models import Topping, Pizza, Writer, Platform, Webtoon
 
 
 @admin.register(Topping)
@@ -11,4 +11,39 @@ class ToppingAdmin(admin.ModelAdmin):
 
 @admin.register(Pizza)
 class PizzaAdmin(admin.ModelAdmin):
+    pass
+
+
+#
+# @admin.register(Customer)
+# class CustomerAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# @admin.register(Shop)
+# class ShopAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# @admin.register(Item)
+# class ItemAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# @admin.register(Shipping)
+# class ShippingAdmin(admin.ModelAdmin):
+#     pass
+
+@admin.register(Writer)
+class WriterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Webtoon)
+class WebtoonAdmin(admin.ModelAdmin):
     pass
--- app/many_to_many/models/m2m_extra_fields_homework.py
+++ app/many_to_many/models/m2m_extra_fields_homework.py
@@ -1,24 +1,76 @@
 from django.db import models
 
 
-class Customer(models.Model):
-    email = models.EmailField(max_length=20)
+#
+# class Customer(models.Model):
+#     email = models.EmailField(max_length=20)
+#     name = models.CharField(max_length=20)
+#     address = models.CharField(max_length=50)
+#
+#     def __str__(self):
+#         return '{email}, {name}'.format(email=self.email, name=self.name)
+#
+#
+# class Shop(models.Model):
+#     name = models.CharField(max_length=20)
+#     customers = models.ManyToManyField(Customer, through='Shipping')
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Item(models.Model):
+#     name = models.CharField(max_length=20)
+#     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Shipping(models.Model):
+#     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+#     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+#     start_date = models.DateTimeField(null=True)
+#     end_date = models.DateTimeField(null=True)
+#
+#     def __str__(self):
+#         return '샵: {shop} ,  고객 : {customer}, 배송출발 : {start_date}, 배송완료 : {end_date}'.format(
+#             shop=self.shop,
+#             customer=self.customer,
+#             start_date=self.start_date if self.start_date else '배송준비중',
+#             end_date=self.end_date if self.end_date else ' '
+#         )
+
+
+class Writer(models.Model):
     name = models.CharField(max_length=20)
-    address = models.CharField(max_length=50)
+    platforms = models.ManyToManyField('Platform', through='Webtoon')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'm2m_writer'
 
 
-class Shop(models.Model):
+class Platform(models.Model):
     name = models.CharField(max_length=20)
-    customers = models.ManyToManyField(Customer, through='Shipping')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'm2m_platform'
 
 
-class Item(models.Model):
-    name = models.CharField(max_length=20)
-    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+class Webtoon(models.Model):
+    writer = models.ForeignKey(Writer, on_delete=models.CASCADE)
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, null=False, blank=False)
 
+    def __str__(self):
+        return f'' \
+               f'({self.writer}, {self.platform}) , {self.title}'
 
-class Shipping(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
-    start_date = models.DateTimeField(null=True)
-    end_date = models.DateTimeField(null=True)
+    class Meta:
+        db_table = 'm2m_webtoon'
